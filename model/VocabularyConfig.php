f73c37636e06338177609e0eb0bf4e86693f974b
Joeli Takala <joeli.takala@helsinki.fi>
2021-09-15 19:37:34 +0300

    Default sidebar view config setting for new concept list

--- model/VocabularyConfig.php
+++ model/VocabularyConfig.php
@@ -440,7 +440,7 @@ class VocabularyConfig extends BaseConfig
         $defview = $this->resource->getLiteral('skosmos:defaultSidebarView');
         if ($defview) {
             $value = $defview->getValue();
-            if ($value === 'groups' || $value === 'hierarchy') {
+            if ($value === 'groups' || $value === 'hierarchy' || $value == 'new') {
                 return $value;
             }
 
